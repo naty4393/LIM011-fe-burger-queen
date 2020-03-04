394a870d6d536f08f33d09fc1280d785d9c69633
naty4393 <naty4393@gmail.com>
2020-03-04 02:26:06 -0500

    uset the testing jest

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,24 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import header1 from '@/components/header1.vue'
+import inputUser from '@/components/input-user.vue'
 
-describe('HelloWorld.vue', () => {
+describe('header1', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const msg = 'Bienvenid@ a Burger Queen'
+    const wrapper = shallowMount(header1, {
       propsData: { msg }
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.text()).toEqual("Bienvenid@ a Burger Queen \n    Estamos juntos, para la mejor atención y recepción de pedidos.")
+  })
+})
+
+describe('inputUser', () => {
+  it('ingresar usuario', () => {
+    const wrapper = shallowMount(inputUser, {
+      data(){
+        
+      }
+    })
+    expect(wrapper.text()).toEqual("Bienvenid@ a Burger Queen \n    Estamos juntos, para la mejor atención y recepción de pedidos.")
   })
 })
