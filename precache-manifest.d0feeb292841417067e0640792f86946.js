afb69bb42930f96e6462f6f18c8f146f65aff700
naty4393 <naty4393@gmail.com>
2020-04-24 11:20:21 -0500

    Updates

---
+++ precache-manifest.d0feeb292841417067e0640792f86946.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa52e97d3b66d49242cb",
+    "url": "/LIM011-fe-burger-queen/css/about.d06692d8.css"
+  },
+  {
+    "revision": "c8c8c2e61b1247e6de57",
+    "url": "/LIM011-fe-burger-queen/css/app.e8812d14.css"
+  },
+  {
+    "revision": "5b7eaaa60ebc6d902fd6",
+    "url": "/LIM011-fe-burger-queen/css/chunk-vendors.78e53563.css"
+  },
+  {
+    "revision": "a68f6fea08ec1f116cfcdb0be16da19d",
+    "url": "/LIM011-fe-burger-queen/img/hamburguesa.a68f6fea.png"
+  },
+  {
+    "revision": "cf94e525ded26c9ce0cae9f48181f0f8",
+    "url": "/LIM011-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "fa52e97d3b66d49242cb",
+    "url": "/LIM011-fe-burger-queen/js/about.fbd93b8e.js"
+  },
+  {
+    "revision": "c8c8c2e61b1247e6de57",
+    "url": "/LIM011-fe-burger-queen/js/app.7405633a.js"
+  },
+  {
+    "revision": "5b7eaaa60ebc6d902fd6",
+    "url": "/LIM011-fe-burger-queen/js/chunk-vendors.2581cfec.js"
+  },
+  {
+    "revision": "373a9a8a2aaf13a05cfc1b6b934a5e51",
+    "url": "/LIM011-fe-burger-queen/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/LIM011-fe-burger-queen/robots.txt"
+  }
+]);
