d759c0eca28cf02c6b2304260a722f2640c9c847
naty4393 <naty4393@gmail.com>
2020-05-04 00:55:44 -0500

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/LIM011-fe-burger-queen/'
-      : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/LIM011-fe-burger-queen/' : '/'
   }
